bb565cdb54b80f59587e9eae473476d3a0611e12
Kay9453 <superkay0101@gmail.com>
2024-06-05 17:51:51 +0800

    Update
    新增：
    1. 新增頁面：
    首頁 (index.html)、部落格(blog.html)、部落格內文(post.html)、服務項目(service.html)、聯絡我(contact.html)、作品集 Modal
    
    更新：
    1. work.html 改用 scss寫法
    2. 把 header & footer 元件化
    3. 更新 header 的 jquery 效果

--- js/all.js
+++ js/all.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
     
-    let count = 0;
+    let open = false;
 
-    // 更換 Menu 圖示使用，不知道為何無效?
-    if( count % 2 == 0){
-        $('.menuBtn-pic').attr('src','images/menu.png');
-    }else{
-        $('.menuBtn-pic').attr('src','images/Close.png');
-    }
-
-    // 
-    $('.menuBtn').click(function(event){
+    $(".navBtn").click(function(event){
 
         //避免觸發 a 標籤：跳轉網頁
         event.preventDefault();
 
         // 選單展開或收起來
-        $('.menu').slideToggle(600);
+        $(".nav").slideToggle(600);
 
-        count = count+1;
+        $(".nav").css("display","flex");
 
-        console.log('我被按了!!'+count);
+        open = !open;
+
+        if (open) {
+            $(".nav-pic").attr("src", "images/Close.png");
+
+        } else {
+            $(".nav-pic").attr("src", "images/menu.png");
+        }
+
     });
 
+    // 偵測寬度
+    // $(window).on('resize', () => {
+    //     if ($( window ).width() >= 992) {
+    //         $(".menu").css('display', '');
+    //     }
+    // })
     
 });
